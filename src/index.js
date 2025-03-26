6e777ec2206bdb108b1a1ec75d83b770a3caf047
JacksonSmith43 <jessica444@gmx.at>
2025-03-26 19:53:12 +0100

    Added the grid.

---
+++ src/index.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+
+    const columns = document.querySelectorAll(".column");
+
+    columns.addEventListener("click", () => {
+        console.log("s");
+    });
+
+
+    function generateRandomNumber() {
+
+    }
+
+
+    function handleCardPress() {
+        console.log("Clicked.");
+    }
+
+    
+});
