7d05621b916d1f92ea1bd746caa900fa219dc5bd
JacksonSmith43 <jessica444@gmx.at>
2025-03-28 18:59:24 +0100

    Created the grids/squares  dynamically.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 document.addEventListener("DOMContentLoaded", (event) => {
 
-    const columns = document.querySelectorAll(".column");
+    const targetColumn = document.querySelector(".column");
 
-    columns.addEventListener("click", () => {
-        console.log("s");
-    });
+    for (let i = 0; i < 36; i++) {
+        const column = document.createElement("div");
+        column.classList.add("single-columns"); // Creates a name for the created element.
+        column.innerHTML = `Column ${i}`;
+        targetColumn.appendChild(column);
+
+        console.log("targetColumn_2: ", targetColumn);
+        console.log("column_2: ", column);
+    }
+
+
+    /*columns.forEach((columns) => {
+        columns.addEventListener("click", () => {
+            console.log("Clicked.");
+            columns.style.display = "none";
+
+        });
+    });*/
 
 
     function generateRandomNumber() {
@@ -16,5 +31,5 @@ document.addEventListener("DOMContentLoaded", (event) => {
         console.log("Clicked.");
     }
 
-    
+
 });
